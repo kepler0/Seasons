5839f38c951e24bdc5051a7f43cec8543c960da0
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-13 00:10:45 -0700

    Season has the basic potential for biome weather manipulation

--- src/main/scala/k2b6s9j/Seasons/traits/Season.scala
+++ src/main/scala/k2b6s9j/Seasons/traits/Season.scala
@@ -1,5 +1,15 @@
 package k2b6s9j.Seasons.traits
 
+import net.minecraft.world.biome.BiomeGenBase
+
 trait Season {
+  
+  def setWeather {
+    for(biome: BiomeGenBase <- BiomeGenBase.biomeList) {
+      if(biome != null) {
+
+      }
+    }
+  }
 
 }
