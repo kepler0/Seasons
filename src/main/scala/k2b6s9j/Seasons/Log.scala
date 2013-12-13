0f04809e3afcd977a9709819a54db7733612342f
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-13 00:56:52 -0700

    Log stuff

---
+++ src/main/scala/k2b6s9j/Seasons/Log.scala
@@ -0,0 +1,30 @@
+package k2b6s9j.Seasons
+
+import java.util.logging.{Level, Logger}
+import cpw.mods.fml.common.FMLLog
+
+object Log {
+
+  def log: Logger = Logger.getLogger("BoatCraft")
+
+  def info(msg:String) {
+    log.setParent(FMLLog.getLogger)
+    log.log(Level.INFO, msg)
+  }
+
+  def warning(msg:String) {
+    log.setParent(FMLLog.getLogger)
+    log.log(Level.WARNING, msg)
+  }
+
+  def severe(msg:String) {
+    log.setParent(FMLLog.getLogger)
+    log.log(Level.SEVERE, msg)
+  }
+
+  def getLogger() {
+    log.setParent(FMLLog.getLogger)
+    log
+  }
+
+}
--- src/main/scala/k2b6s9j/Seasons/Seasons.scala
+++ src/main/scala/k2b6s9j/Seasons/Seasons.scala
@@ -3,12 +3,19 @@ package k2b6s9j.Seasons
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent}
+import k2b6s9j.Seasons.months.December
 
 @Mod(name = "Seasons", modid = "seasons", version = "ModJam", modLanguage = "scala")
 object Seasons {
 
   @EventHandler
   def preInit(event: FMLPreInitializationEvent) {
+
+    Log.info("Seasons")
+    Log.info("Copyright Kepler Sticka-Jones 2013")
+
+    Log.info("Currently test the month of December globally.")
+    December.setWeather
 
   }
 
