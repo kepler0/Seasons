596530b65e791a702109863f5b69c07cff78d904
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-12 23:11:07 -0700

    Mod class!

---
+++ src/main/scala/k2b6s9j/Seasons/Seasons.scala
@@ -0,0 +1,25 @@
+package k2b6s9j.Seasons
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent}
+
+@Mod(name = "Seasons", modid = "seasons", version = "ModJam", modLanguage = "scala")
+object Seasons {
+
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent) {
+
+  }
+
+  @EventHandler
+  def Init(event: FMLInitializationEvent) {
+
+  }
+
+  @EventHandler
+  def postInit(event: FMLPostInitializationEvent) {
+
+  }
+
+}
