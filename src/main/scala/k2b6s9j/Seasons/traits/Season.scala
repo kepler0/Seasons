5868aaa1013e8956fc8d68f85872a83671ad5b6a
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-13 21:18:15 -0700

    Disable precipitation in the desert

--- src/main/scala/k2b6s9j/Seasons/traits/Season.scala
+++ src/main/scala/k2b6s9j/Seasons/traits/Season.scala
@@ -1,12 +1,16 @@
 package k2b6s9j.Seasons.traits
 
 import net.minecraft.world.biome.BiomeGenBase
+import net.minecraftforge.common.BiomeDictionary
 
 trait Season {
   
   def setWeather() {
     for(biome: BiomeGenBase <- BiomeGenBase.biomeList) {
       if(biome != null) {
+        if (BiomeDictionary.isBiomeOfType(biome, DESERT)) {
+          biome.setDisableRain()
+        }
         if (canSnow) {
           biome.setDisableRain().setEnableSnow()
         }
