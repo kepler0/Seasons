d8fd6821f5836122218c330b66ecdbc52301e376
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-13 22:12:05 -0700

    Break the loop if in the biome type blacklist.

--- src/main/scala/k2b6s9j/Seasons/traits/Season.scala
+++ src/main/scala/k2b6s9j/Seasons/traits/Season.scala
@@ -2,6 +2,7 @@ package k2b6s9j.Seasons.traits
 
 import net.minecraft.world.biome.BiomeGenBase
 import net.minecraftforge.common.BiomeDictionary
+import scala.util.control.Breaks._
 
 trait Season {
   
@@ -10,21 +11,27 @@ trait Season {
       if(biome != null) {
         if (BiomeDictionary.isBiomeOfType(biome, BiomeDictionary.Type.WATER)) {
           //I would rather not freeze over the oceans
+          break()
         }
         if (BiomeDictionary.isBiomeOfType(biome, BiomeDictionary.Type.DESERT)) {
           biome.setDisableRain()
+          break()
         }
         if (BiomeDictionary.isBiomeOfType(biome, BiomeDictionary.Type.FROZEN)) {
           //And let's not melt any frozen biomes, they're frozen for a reason.
+          break()
         }
         if (BiomeDictionary.isBiomeOfType(biome, BiomeDictionary.Type.NETHER)) {
           //Do not under any circumstances freeze over hell.
+          break()
         }
         if (BiomeDictionary.isBiomeOfType(biome, BiomeDictionary.Type.END)) {
           //I would rather not piss of those of the Ender realm.
+          break()
         }
         if (BiomeDictionary.isBiomeOfType(biome, BiomeDictionary.Type.MUSHROOM)) {
           //I will not touch the majestic mushroom biomes. Plus, they kinda scare me.
+          break()
         }
         else {
           //Ok. No that we've ran through the black list...
