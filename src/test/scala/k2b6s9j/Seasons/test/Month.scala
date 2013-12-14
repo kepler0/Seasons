845c73ec93e9d797da0eff634a6e611d202a8a7e
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-14 16:30:20 -0700

    Begin to test for temperature

--- src/test/scala/k2b6s9j/Seasons/test/Month.scala
+++ src/test/scala/k2b6s9j/Seasons/test/Month.scala
@@ -12,48 +12,96 @@ class Month extends FlatSpec with Matchers {
     January.daysInMonth should be (31)
   }
 
+  it should "have a temperature" in {
+
+  }
+
   "February" should "have 28 days." in {
     February.daysInMonth should be (28)
+  }
+
+  it should "have a temperature" in {
+
   }
 
   "March" should "have 31 days." in {
     March.daysInMonth should be (31)
   }
 
+  it should "have a temperature" in {
+
+  }
+
   "April" should "have 30 days." in {
     April.daysInMonth should be (30)
+  }
+
+  it should "have a temperature" in {
+
   }
 
   "May" should "have 31 days." in {
     May.daysInMonth should be (31)
   }
 
+  it should "have a temperature" in {
+
+  }
+
   "June" should "have 30 days." in {
     June.daysInMonth should be (30)
+  }
+
+  it should "have a temperature" in {
+
   }
 
   "July" should "have 31 days." in {
     July.daysInMonth should be (31)
   }
 
+  it should "have a temperature" in {
+
+  }
+
   "August" should "have 31 days." in {
     August.daysInMonth should be (31)
+  }
+
+  it should "have a temperature" in {
+
   }
 
   "September" should "have 30 days." in {
     September.daysInMonth should be (30)
   }
 
+  it should "have a temperature" in {
+
+  }
+
   "October" should "have 31 days." in {
     October.daysInMonth should be (31)
+  }
+
+  it should "have a temperature" in {
+
   }
 
   "November" should "have 30 days." in {
     November.daysInMonth should be (30)
   }
 
+  it should "have a temperature" in {
+
+  }
+
   "December" should "have 31 days." in {
     December.daysInMonth should be (31)
+  }
+
+  it should "have a temperature" in {
+
   }
 
 }
