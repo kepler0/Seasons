b2dcfe3491dcdafd57b86fa425901b5418d69861
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-14 16:06:31 -0700

    Int does not take parameters

--- src/test/scala/k2b6s9j/Seasons/test/Month.scala
+++ src/test/scala/k2b6s9j/Seasons/test/Month.scala
@@ -6,51 +6,51 @@ import k2b6s9j.Seasons.months._
 object Month extends FlatSpec with Matchers {
 
   "January" should "have 31 days." in {
-    January.daysInMonth() should be (31)
+    January.daysInMonth should be (31)
   }
 
   "February" should "have 28 days." in {
-    February.daysInMonth() should be (28)
+    February.daysInMonth should be (28)
   }
 
   "March" should "have 31 days." in {
-    March.daysInMonth() should be (31)
+    March.daysInMonth should be (31)
   }
 
   "April" should "have 30 days." in {
-    April.daysInMonth() should be (30)
+    April.daysInMonth should be (30)
   }
 
   "May" should "have 31 days." in {
-    May.daysInMonth() should be (31)
+    May.daysInMonth should be (31)
   }
 
   "June" should "have 30 days." in {
-    June.daysInMonth() should be (30)
+    June.daysInMonth should be (30)
   }
 
   "July" should "have 31 days." in {
-    July.daysInMonth() should be (31)
+    July.daysInMonth should be (31)
   }
 
   "August" should "have 31 days." in {
-    August.daysInMonth() should be (31)
+    August.daysInMonth should be (31)
   }
 
   "September" should "have 30 days." in {
-    September.daysInMonth() should be (30)
+    September.daysInMonth should be (30)
   }
 
   "October" should "have 31 days." in {
-    October.daysInMonth() should be (31)
+    October.daysInMonth should be (31)
   }
 
   "November" should "have 30 days." in {
-    November.daysInMonth() should be (30)
+    November.daysInMonth should be (30)
   }
 
   "December" should "have 31 days." in {
-    December.daysInMonth() should be (31)
+    December.daysInMonth should be (31)
   }
 
 }
