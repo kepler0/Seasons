c4eff5f6f50e14d06953056c325bd7cf1f3a84f0
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-15 01:07:57 -0700

    Logic!

--- src/main/scala/k2b6s9j/Seasons/TimeManager.scala
+++ src/main/scala/k2b6s9j/Seasons/TimeManager.scala
@@ -1,11 +1,21 @@
 package k2b6s9j.Seasons
 
 import k2b6s9j.Seasons.months._
+import k2b6s9j.Seasons.traits.Month
 
 object TimeManager {
 
   def ticksInYear(): Int = January.ticksInMonth + February.ticksInMonth + March.ticksInMonth + April.ticksInMonth + May.ticksInMonth + June.ticksInMonth + July.ticksInMonth + August.ticksInMonth + September.ticksInMonth + October.ticksInMonth + November.ticksInMonth + December.ticksInMonth
 
   def checkDivisibility(num:Int, divisor:Int) = num % divisor == 0
+
+  def findMonth(totalTicks: Int): Month = {
+    if(totalTicks > ticksInYear()) {
+
+    }
+    else {
+
+    }
+  }
 
 }
