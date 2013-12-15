fa565d5f60afaf4528c4ad315af0a334869d1048
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-14 21:14:26 -0700

    Calculating the ticks in a year!

--- src/main/scala/k2b6s9j/Seasons/TimeManager.scala
+++ src/main/scala/k2b6s9j/Seasons/TimeManager.scala
@@ -1,5 +1,11 @@
 package k2b6s9j.Seasons
 
+import k2b6s9j.Seasons.months._
+
 object TimeManager {
+
+  def ticksInYear(): Int = {
+    January.ticksInMonth + February.ticksInMonth + March.ticksInMonth + April.ticksInMonth + May.ticksInMonth + June.ticksInMonth + July.ticksInMonth + August.ticksInMonth + September.ticksInMonth + October.ticksInMonth + November.ticksInMonth + December.ticksInMonth
+  }
 
 }
