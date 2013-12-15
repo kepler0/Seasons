3a1d97d72613a4403774177cb9fec071adf52a8d
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-14 18:16:07 -0700

    should it be a fmllog?

--- src/test/scala/k2b6s9j/Seasons/test/LogTest.scala
+++ src/test/scala/k2b6s9j/Seasons/test/LogTest.scala
@@ -4,13 +4,13 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, FlatSpec}
 import k2b6s9j.Seasons.Log
-import java.util.logging.Logger
+import cpw.mods.fml.common.FMLLog
 
 @RunWith(classOf[JUnitRunner])
 class LogTest extends FlatSpec with Matchers {
 
   "The Mod Logger" should "return a logger." in {
-    Log.getLogger() shouldBe a [Logger]
+    Log.getLogger() shouldBe a [FMLLog]
   }
 
 }
