50c70d9f4a43ea38f1465aa1b4c7f1938227e695
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-14 18:07:40 -0700

    Test the return of the log

---
+++ src/test/scala/k2b6s9j/Seasons/test/Log.scala
@@ -0,0 +1,16 @@
+package k2b6s9j.Seasons.test
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, FlatSpec}
+import k2b6s9j.Seasons.Log
+import java.util.logging.Logger
+
+@RunWith(classOf[JUnitRunner])
+class Log extends FlatSpec with Matchers {
+
+  "The Mod Logger" should "return a logger." in {
+    Log.getLogger() should be (Logger)
+  }
+
+}
