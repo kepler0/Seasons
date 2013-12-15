f8af53433afa6329dc81690efe3c0f5c1c29762a
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-15 00:54:45 -0700

    Simplified Divisibility methods

--- src/main/scala/k2b6s9j/Seasons/TimeManager.scala
+++ src/main/scala/k2b6s9j/Seasons/TimeManager.scala
@@ -6,17 +6,6 @@ object TimeManager {
 
   def ticksInYear(): Int = January.ticksInMonth + February.ticksInMonth + March.ticksInMonth + April.ticksInMonth + May.ticksInMonth + June.ticksInMonth + July.ticksInMonth + August.ticksInMonth + September.ticksInMonth + October.ticksInMonth + November.ticksInMonth + December.ticksInMonth
 
-  def divisibleBy1(num:Int) = num % 1 == 0
-  def divisibleBy2(num:Int) = num % 2 == 0
-  def divisibleBy3(num:Int) = num % 3 == 0
-  def divisibleBy4(num:Int) = num % 4 == 0
-  def divisibleBy5(num:Int) = num % 5 == 0
-  def divisibleBy6(num:Int) = num % 6 == 0
-  def divisibleBy7(num:Int) = num % 7 == 0
-  def divisibleBy8(num:Int) = num % 8 == 0
-  def divisibleBy9(num:Int) = num % 9 == 0
-  def divisibleBy10(num:Int) = num % 10 == 0
-  def divisibleBy11(num:Int) = num % 11 == 0
-  def divisibleBy12(num:Int) = num % 12 == 0
+  def checkDivisibility(num:Int, divisor:Int) = num % divisor == 0
 
 }
