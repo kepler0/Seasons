bb53e148ed39c197fb47dffd93e5421a1dd23da5
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-14 21:24:29 -0700

    Damn it...

--- src/test/scala/k2b6s9j/Seasons/test/TimeManagerTest.scala
+++ src/test/scala/k2b6s9j/Seasons/test/TimeManagerTest.scala
@@ -1,8 +1,12 @@
 package k2b6s9j.Seasons.test
 
 import k2b6s9j.Seasons.TimeManager
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, FlatSpec}
 
-class TimeManagerTest {
+@RunWith(classOf[JUnitRunner])
+class TimeManagerTest extends FlatSpec with Matchers {
 
   "A year" should "last 8,016,000 ticks." in {
     TimeManager.ticksInYear() should be (8016000)
